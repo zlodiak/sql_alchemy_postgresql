7e406f2dfaed287840e1961a58a724589eea0f47
kalinin <prozaik81-2@yandex.ru>
2020-02-13 21:19:13 +0300

    pg example

---
+++ index.py
@@ -0,0 +1,37 @@
+import random
+from sqlalchemy import create_engine, Table, MetaData, select, Column, String, Integer
+from sqlalchemy.sql import and_, or_, not_
+import psycopg2
+
+engine = create_engine('postgresql://alchemy_user:qwerty@localhost:5432/db_alchemy')
+connection = engine.connect()
+metadata = MetaData()
+
+quotes = Table('quotes', metadata,
+    Column('id', Integer, primary_key=True),
+    Column('text', String, nullable=False),
+    Column('weight', Integer, nullable=False)
+)
+metadata.create_all(engine)
+
+
+quotes_json = []
+with open("text.txt") as file_handler:
+    for line in file_handler:
+        quotes_json.append({
+            'text': line,
+            'weight': random.randint(1, 10),
+        })
+connection.execute(quotes.insert(), quotes_json)
+
+
+s = select([quotes]).where(and_(quotes.c.weight > 3, quotes.c.weight < 8))
+result = connection.execute(s)
+for row in result:
+    print(row['id'], row['text'][:20] + '...', row['weight'])
+
+
+
+
+
+
